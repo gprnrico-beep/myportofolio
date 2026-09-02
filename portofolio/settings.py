6f3c7da9e4fade05d7d538c8afe3c03ace28b0e1
gprnrico-beep <gprn.rico@gmail.com>
2026-09-02 21:39:23 +0700

    Remove duplicate ALLOWED_HOSTS override

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d%nzs2xn@av(-va!-63apljj7=e!zcb@+0(lwo7t2ds3y#6-@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
